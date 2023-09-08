38c8cf089564c58175e32dec9f4e309866fc74f1
Blu William Opland <bluw2007@gmail.com>
2023-09-08 09:03:16 +0200

    Changed css for articles

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+
+
+function getProjects() {
+    /* get files in directory, return list of relative filepaths for each html file in Projects Folder */
+
+
+
+
+
+
+}
