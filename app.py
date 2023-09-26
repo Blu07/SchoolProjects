6db2d8673b0c74cc2e31bb0ab1d2f937c736761d
Blu07 <bluw2007@gmail.com>
2023-09-26 18:09:55 +0200

    revert changes

--- app.py
+++ app.py
@@ -110,16 +110,8 @@ def serve_project(project_path):
 
 if __name__ == '__main__':
     
-
-    parser = argparse.ArgumentParser(prog='Run application')
-
-    parser.add_argument('-f', '--full', action='store_true', help='Run application with a full scss preprocess before.')
-    parser.add_argument('-n', '--normal', action='store_true', help='Run application as normal. Default.', default=True)
+    debug = True
     
-    args = parser.parse_args()
-
-
-
     def open_sass_watcher_thread():
         # Function to create the thread holding the SASS watcher
         def sass_watcher_thread():
@@ -131,15 +123,13 @@ if __name__ == '__main__':
         watcher_thread.daemon = True  # Exit the thread when the main program exits
         watcher_thread.start()
         
-    _ = "New-Item -Path $profile -Type File -Force"
+    # New-Item -Path $profile -Type File -Force
 
-    if args.full:
-        do_full_sass(scss_Folder, css_Folder)
 
     open_sass_watcher_thread()    
 
     # Run the app
-    app.run(debug=True)
+    app.run(debug=debug)
 
 
 
