a1e59a399b781d8bda1c2efc7d547703853e6656
Blu07 <bluw2007@gmail.com>
2023-12-05 15:00:19 +0100

    work on arrays in class

---
+++ static/js/arrays.js
@@ -0,0 +1,64 @@
+// let drikke = ["kaffe", "te", "cola", "melk"];
+// let mat = ["taco", "pizza", "hamburger", "pølse"];
+// let matListe2 = ["Hei", "På", "Deg"];
+// mat.splice(2, 0, matListe2[2]);
+
+// console.log(drikke, mat);
+
+
+// Oppgave 1
+// Lag en array med tallene 0, 1, 2, 3, 4, 6, 7, 8, 9 og 10 og gi den et passende navn.
+
+let nullTilTi = [0, 1, 2, 3, 4, 6, 7, 8, 9, 10]
+
+
+// Oppgave 2
+// Skriv ut alle partallene fra arrayen i konsollen.
+
+
+for (const num in nullTilTi) {
+    if (num % 2 === 0) {
+        console.log(num)
+    }
+}
+
+
+// Oppgave 3
+// Lag arrayen let tall = [2, 4, 6, 8] ;.
+
+let tall = [2, 4, 6, 8]
+
+// a)	Bruk metodene vi har sett på, og gjør om arrayen tall til [4, 6] .
+
+tall.shift()
+tall.pop()
+
+// b)	Legg til tallet 5 mellom tallene 4 og 6, slik at arrayen nå inneholder [4, 5, 6] .
+
+tall.splice(1, 0, 5)
+
+// c)	Gjør om arrayen slik at den inneholder [3, 4, 5, 6, 7] .
+
+tall.unshift(3)
+tall.push(7)
+
+// d)	Gjør om arrayen slik at den inneholder ["tre", 4, "fem", 6, "syv"] .
+
+bokstavNavn = ["null", "en", "to", "tre", "fire", "fem", "seks", "syv", "åtte", "ni", "ti"]
+
+tall.forEach(function (item, index) {
+    if (item % 2 == 1) {
+        tall.splice(index, 1, bokstavNavn[item])
+    }
+});
+
+
+// e)   Vis veriden av tall i consollen.
+
+console.log(tall)
+
+
+// Oppgave 4
+// Ta igjen utgangspunkt i arrayen du laget i forrige oppgave.
+// a)	Hva tror du skjer hvis du skriver tall.indexOf(10) eller tall.lastIndexOf(10) ?
+// b)	Prøv ut begge variantene og se hva du får. Hvordan tolker du resultatet?
